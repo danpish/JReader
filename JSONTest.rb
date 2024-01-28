236cfdb9141bb24ccc5500322d951ba108ea084e
Faniel Dishyar <winemu@daniel.com>
2024-01-28 14:48:41 +0330

    first functional preview

--- JSONTest.rb
+++ JSONTest.rb
@@ -1,11 +1,12 @@
-require 'json'
+require "json"
+
+$randfunc = Random.new()
 
 js_parsed = JSON.load File.new "download.json"
 
 #puts "#{js_parsed.keys}"
 
 #puts "#{js_parsed["info"]}"
-
 
 #puts "#{js_parsed["links"].length}"
 
@@ -15,15 +16,19 @@ js_parsed = JSON.load File.new "download.json"
 #    puts "#{js_parsed["links"][links]} \n"
 #end
 
+instance = "http://teddit.zaggy.nl"
+rand_link = $randfunc.rand(js_parsed["links"].length)
+$image_url = instance + js_parsed["links"][rand_link]["images"]["preview"]
+puts $image_url
 
 
-#Shoes.app width: 300, height: 200 do
-#  background lime..blue
-#
-#  stack do
-#    para "Welcome to the world of Shoes!"
-#    button "Click me" do alert "Nice click!" end
-#    image "http://shoesrb.com/img/shoes-icon.png",
-#          margin_top: 20, margin_left: 10
-#  end
-#end
+Shoes.app width: 300, height: 200 do
+    background lime..blue
+    
+    stack do
+        para "Welcome to the world of Shoes!"
+        button "Click me" do alert "Nice click!" end
+        image $image_url,
+            margin_top: 20, margin_left: 10
+    end
+end
