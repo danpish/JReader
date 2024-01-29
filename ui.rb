2ae67a4904c56f15531a40f2d1306c48b056dbe1
Faniel Dishyar <winemu@daniel.com>
2024-01-29 15:32:49 +0330

    final start page design

--- ui.rb
+++ ui.rb
@@ -2,10 +2,17 @@ require "./core"
 
 Shoes.app width: 800, height: 600 do
     #startup page
+    background "#123"
     flow width: "20%" do end
-    flow width: "60%" do
-        stack width:"100%", height: 200 do
-            button "woow", width: "100%", height: "100%" do alert "it works" end
+    flow width: "60%", margin: 10 do
+        stack width: "100%", height: 200,align: "center" do
+            background "#256", curve: 24
+            flow height: "30%" do end
+            flow align: "center", margin: 10 do
+                para "search something",align: "center"
+                edit_line width: -75,align: "center"
+                button "search", width: 75,align: "center"
+            end
         end
     end
     flow width: "20%" do end
