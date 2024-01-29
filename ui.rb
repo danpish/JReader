a91bca97fb37fab61daa2b81445941efe06959ca
Faniel Dishyar <winemu@daniel.com>
2024-01-29 14:48:35 +0330

    gui init

--- ui.rb
+++ ui.rb
@@ -1,2 +1,12 @@
 require "./core"
 
+Shoes.app width: 800, height: 600 do
+    #startup page
+    flow width: "20%" do end
+    flow width: "60%" do
+        stack width:"100%", height: 200 do
+            button "woow", width: "100%", height: "100%" do alert "it works" end
+        end
+    end
+    flow width: "20%" do end
+end
