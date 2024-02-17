880206e42c26f9f36a8e0ac6c2c426477e3105d1
danpish <94285332+danpish@users.noreply.github.com>
2024-02-17 12:32:25 +0330

    Remove benchmark

--- ui.rb
+++ ui.rb
@@ -14,12 +14,6 @@ $rects = Array.new()
 
 $rand = Random.new()
 
-for x in 1..50 do
-  $rects.push (Rectangle.new(100, 100, Gosu::Color::YELLOW))
-  $rects[x - 1].corner_data([$rand.rand(10), $rand.rand(10),$rand.rand(10),$rand.rand(10)])
-end
-
-
 class JReader < Gosu::Window
   def initialize
     super $width, $height
@@ -29,7 +23,6 @@ class JReader < Gosu::Window
     $cool.make(100, 100)
     $rect.make(200,150)
     $rect2.make(300,300)
-    $rects.each { |rect| rect.make($rand.rand(800), $rand.rand(600))}
     puts Gosu.fps
   end
 end
